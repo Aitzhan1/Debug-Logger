e3716a8f671669b6957df7ab986fe079efb1804b
Aitzhan <aitmuhan0@gmail.com>
2018-11-23 09:34:56 +0600

    Initial

---
+++ Assets/Scripts/FileLoger.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class FileLoger : MonoBehaviour
+{
+    private string output = "";
+    private string stack = "";
+    private string path;
+    private StreamWriter sw;
+    public bool stackActive = false;
+    
+
+    private void Start()
+    {
+        path = Application.dataPath + "/StreamingAssets/Debug.txt";
+        sw = new StreamWriter(path);
+      
+    }
+    void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        output = logString;
+        stack = stackTrace;
+
+        
+       
+        sw.WriteLine("\n"+output);
+        if (stackActive)
+        {
+            sw.WriteLine(stack);
+        }
+     
+    }
+    private void OnApplicationQuit()
+    {
+        sw.Close();
+    }
+}
